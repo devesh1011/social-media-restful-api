8e42327c061c271b53b6d7c57569f70157c87ba8
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-02-29 17:18:42 +0530

    passport config file

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+require("dotenv").config();
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(options, async (jwt_payload, done) => {
+    try {
+      const user = await User.findOne({ _id: jwt_payload.sub });
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (error) {
+      return done(err, false);
+    }
+  })
+);
+
+module.exports = passport;
