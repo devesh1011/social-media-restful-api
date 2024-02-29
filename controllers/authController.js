6e8da79eefef11ae990ef7066d31f8e4a82eb9cf
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-02-29 17:16:49 +0530

    login in the authController and some files added

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const register = async (req, res) => {};
+
+const login = async (req, res) => {};
+
+const logout = async (req, res) => {};
+
+const refresh = async (req, res) => {};
+
+module.exports = {
+  refresh,
+  register,
+  login,
+  logout,
+};
---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const userProfile = async (req, res) => {};
+
+const updateUserProfile = async (req, res) => {};
+
+const deleteUserProfile = async (req, res) => {};
+
+module.exports = {
+  userProfile,
+  updateUserProfile,
+  deleteUserProfile,
+};
