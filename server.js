c4b19c2c2352c1ab74c54c44c5bd6d189e4b0238
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-03-05 10:43:25 +0530

    cors added to server.js

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const connectDB = require("./config/db");
 const path = require("path");
 const CustomError = require("./middleware/CustomError");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const corsOptions = require("./config/corsConfig");
 require("dotenv").config();
 require("./config/passport");
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
